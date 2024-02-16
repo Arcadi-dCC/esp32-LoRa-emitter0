fc7139057a5b807481fdfdc759a1129d5ed9fec6
Arcadi del Castillo <98622132+Arcadi-dCC@users.noreply.github.com>
2024-02-16 20:28:11 +0100

    LBT added
    listen before talking feature included as function "isChannelBusy(void)". The function checks if the channel is busy. If so, the code sleeps for a few seconds and retries to send.
    
    printStr function copied from gateway project.

--- emitter/emitter.ino
+++ emitter/emitter.ino
@@ -12,9 +12,14 @@
 #define NET_ID 0x53AC ////ID that all emmiters must use to communicate with gateway
 
 RTC_DATA_ATTR byte out_packet [3] = {(NET_ID & 0xFF00) >> 8, NET_ID & 0x00FF, 0}; 
+boolean channel_busy = true;
+boolean Cad_isr_responded = false;
 
 int LoRaConfig(int sck, int miso, int mosi, int ss, int rst, int dio0, int freq, int sf, int bw);
 void SwReset(int countdown);
+boolean isChannelBusy(void);
+void onCadDone(boolean signalDetected);
+void printStr(byte* str, int len);
 void onTxDone(void);
 
 void setup() {
@@ -28,17 +33,21 @@ void setup() {
   {
     SwReset(10);
   }
+  LoRa.onCadDone(onCadDone);
   //LoRa.onTxDone(onTxDone);
   
   delay(1000);
 
+  //Retry in a few seconds if channel is busy
+  if(isChannelBusy())
+  {
+    Serial.println("Channel is busy. Retrying in a few seconds");
+    esp_deep_sleep(random(10,14)*1000000);
+  }
+
   // send packet
   Serial.print("Sending: ");
-  for (int i = 0; i<3;i++)
-  {
-    Serial.print(out_packet[i]);
-    Serial.print(" | ");
-  }
+  printStr(out_packet, sizeof(out_packet));
   Serial.println();
   while(!LoRa.beginPacket());
   //LoRa.print("0");                //Configurat per enviar 0 sempre (per no anar variant el tamany dels paquets)
@@ -83,6 +92,34 @@ int LoRaConfig(int sck, int miso, int mosi, int ss, int rst, int dio0, int freq,
   return 0;  
 }
 
+//Checks if someone is using the configured channel. Returns true if used, false if free
+boolean isChannelBusy(void)
+{
+  Cad_isr_responded = false;
+  LoRa.channelActivityDetection();
+  while(!Cad_isr_responded)
+  {
+    delay(1);
+  }
+  return channel_busy;
+}
+
+void onCadDone(boolean signalDetected) //true = signal detected
+{
+  channel_busy = signalDetected;
+  Cad_isr_responded = true;
+}
+
+void printStr(byte* str, int len)
+{
+  Serial.print("| ");
+  for (int w = 0; w < len; w++)
+  {
+    Serial.print(str[w]);
+    Serial.print(" | ");
+  }
+}
+
 //Performs a software reset after warning through the serial output and doing a countdown.
 void SwReset(int countdown)
 {
@@ -96,7 +133,7 @@ void SwReset(int countdown)
   ESP.restart();
 }
 
-//Called when the sending of data is finished.
+//Called when the sending of data is finished. Currently not used
 void onTxDone(void)
 {
   //sleep for 1'
